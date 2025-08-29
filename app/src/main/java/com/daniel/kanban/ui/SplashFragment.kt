4bb1edc70971db99d14e21515d88d0b149ee4a64
Daniel Christi Cardoso Bento <20232TIIMI0365@cefetes.br>
2025-08-29 09:02:06 -0300

    passo 62

---
+++ app/src/main/java/com/daniel/kanban/ui/SplashFragment.kt
@@ -0,0 +1,44 @@
+
+package com.daniel.kanban.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.daniel.kanban.R
+import com.daniel.kanban.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View{
+        _binding = FragmentSplashBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({checkAuth()}, 3000)
+    }
+    private fun checkAuth(){
+        findNavController().navigate(R.id.action_splashFragment_to_Autentication)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
---
+++ app/src/main/java/com/daniel/kanban/ui/auth/RegisterFragment.kt
@@ -0,0 +1,30 @@
+package com.daniel.kanban.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.daniel.kanban.R
+import com.daniel.kanban.databinding.FragmentRegisterBinding
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
