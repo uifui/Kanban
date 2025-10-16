5190593e0932a2903d67096ad62c0ede61496b11
20232tiimi0365 <danielchristicar@gmail.com>
2025-10-16 09:01:28 -0300

    Registro finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-parcelize")
     id("com.google.gms.google-services")
 }
 
@@ -48,10 +49,12 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
-    implementation(platform("com.google.firebase:firebase-auth:24.0.1"))
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+    implementation("com.google.firebase:firebase-auth")
 }
--- app/src/main/java/com/daniel/kanban/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/daniel/kanban/ui/auth/RegisterFragment.kt
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.daniel.kanban.R
 import com.daniel.kanban.databinding.FragmentRegisterBinding
-import com.google.
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var auth: FirebaseAuth
 
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +29,63 @@ class RegisterFragment : Fragment() {
     ): View {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+
+
+    }
+    private fun validateData(){
+        val email = binding.editEmail.text.toString().trim()
+        val senha = binding.editSenha.text.toString().trim()
+
+        if (email.isNotBlank() and senha.isNotBlank()){
+            binding.progressBar.isVisible = true
+            registerUser(email, senha)
+            Toast.makeText(
+                requireContext(),
+                "entrou1",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        else{
+            //mostrar mensagem de erro
+
+        }
+    }
+
+    private fun initListener(){
+        binding.btnRegister.setOnClickListener {
+            validateData()
+        }
+    }
+
+    private fun registerUser(email: String, password: String){
+
+        try {
+
+            val auth = FirebaseAuth.getInstance()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //mensagem de sucesso
+                        findNavController().navigate(R.id.action_global_homeFragment)
+                    } else {
+                        //mensagem de erro
+                        Toast.makeText(
+                            requireContext(),
+                            task.exception?.message,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+        catch (e: Exception){
+            Toast.makeText(requireContext(), e.message.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
